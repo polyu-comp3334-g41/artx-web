538965d34ff165e25e04f24cda3797079f79aba7
gcctbilly <gcctxhl@163.com>
2022-04-12 20:58:16 +0800

    Initialze the structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import {Route,Navigate, Routes} from 'react-router-dom'
+
+export default class App extends Component {
+    render() {
+        return(
+            <div>
+                <Routes>
+                </Routes>
+            </div>
+        )
+    }
+}
