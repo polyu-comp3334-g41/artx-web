1a91ed443a56403b4b1e90d2a7f822fe4a69616a
gcctbilly <gcctxhl@163.com>
2022-04-12 22:12:30 +0800

    Swap Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 import {Route,Navigate, Routes} from 'react-router-dom'
+import Home from './page/Home/Home'
+import Main from './page/Main/Main'
+import Swap from './page/Swap/Swap'
 
 export default class App extends Component {
     render() {
         return(
             <div>
                 <Routes>
+                  <Route path="main" element={<Main/>}>
+                    <Route path="home" element={<Home/>}/>
+                    <Route path="swap" element={<Swap/>}/>
+                    <Route path="" element = {<Navigate to="home" />} /> 
+                  </Route>
+                  <Route path="*" element = {<Navigate to="main" />} /> 
                 </Routes>
             </div>
         )
